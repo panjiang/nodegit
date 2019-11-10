439f6c2da2626bfaba3d3f1cff1e814c86235dc5
root <root@J.localdomain>
2019-11-10 23:22:23 +0800

    Export merge file function.

--- test/mergefile.js
+++ test/mergefile.js
@@ -2,36 +2,62 @@ var NodeGit = require('..');
 var path = require('path')
 var fse = require('fs-extra')
 
-if(process.env.NODEGIT_TEST_THREADSAFETY) {
-  console.log('Enabling thread safety in NodeGit');
-  NodeGit.enableThreadSafety();
-} else if (process.env.NODEGIT_TEST_THREADSAFETY_ASYNC) {
-  console.log('Enabling thread safety for async actions only in NodeGit');
-  NodeGit.setThreadSafetyStatus(NodeGit.THREAD_SAFETY.ENABLED_FOR_ASYNC_ONLY);
-}
+const dir = '/mnt/d/workspace/nodegit/test';
 
 async function newMergeFileInput(filePath) {
   const stat = await fse.stat(filePath);
-  const content = await fse.readFile(filePath);
+  const content = await fse.readFile(filePath, 'utf-8');
 
-  return {
-    version: 0,
-    ptr: content,
-    size: content.length,
-    path: filePath,
-    mode: stat.mode,
-  };
+  // Wrong: It will case Crash.
+  // const input = {
+  //   ptr: content,
+  //   size: content.length,
+  //   mode: stat.mode,
+  //   path: filePath,
+  // }
+
+  // Correct: Must create object from NodeGit class.
+  const input = new NodeGit.MergeFileInput();
+  input.ptr = content
+  input.size = content.length
+  input.mode = stat.mode
+  input.path = filePath
+
+  return input;
 }
 
 function getFilePath(fileName) {
-  return path.join('/Users/mac/Library/Application Support/Electron/notedown/e5dcc97d-d105-41dc-bc49-70d0f3d5fa19/test', fileName)
+  return path.join(dir, fileName)
 }
 
-async function test(){
+async function test() {
+
+  // 3 files.
   const base = await newMergeFileInput(getFilePath('base.md'));
   const ours = await newMergeFileInput(getFilePath('ours.md'));
   const theirs = await newMergeFileInput(getFilePath('theirs.md'));
-  console.log(NodeGit.Merge.file(base, ours, theirs, {}))
+
+  console.log({ base, ours, theirs })
+
+  // Call merge file function.
+  const res = await NodeGit.Merge.file(base, ours, theirs, {
+    version: 0,
+    ancestorLabel: 'BASE',
+    ourLabel: 'OUR',
+    theirLabel: 'THEIR',
+    favor: 0,
+    flags: 0,
+    markerSize: 10
+  });
+
+  // Must call function to get field.
+  console.log("res:", {
+    automergeable: res.automergeable(),
+    path: res.path(),
+    mode: res.mode(),
+    ptr: res.ptr(),
+    len: res.len()
+  })
 }
 
 
